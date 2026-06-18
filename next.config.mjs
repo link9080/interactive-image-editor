0e65f749b002323542a3aea7b5bbadc1c31b3381
link9080 <oldmetalgeat@gmail.com>
2026-06-18 21:12:55 +0900

    ビルドpages公開用に修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // ✨ これを追加（静的HTMLとして出力）
+  basePath: '/interactive-image-editor', // ✨ これを追加（リポジトリ名を指定）
+
   typescript: {
     ignoreBuildErrors: true,
   },
